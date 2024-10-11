ca013f1f17e8f881ac2d6b083a66283919b685ca
Kirill Kuznetsov <kka.fullstack@gmail.com>
2024-10-11 17:22:44 +0300

    fix api url

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,8 +1,8 @@
 import { env } from '~shared/lib/env';
 
 export function baseUrl(path: string) {
-  const url = env('VITE_BACKEND_HOST', 'localhost');
-  const port = env('VITE_BACKEND_PORT', '80');
+  const url = env('VITE_API_URL', 'localhost');
+  const port = env('VITE_API_PORT', '80');
   const method = port === '443' ? 'https' : 'http';
 
   return `${method}://${url}:${port}/api${path}`;
