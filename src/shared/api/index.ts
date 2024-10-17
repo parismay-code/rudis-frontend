8f2e8d19e030a38843d16aab950dd1d07eb40b0e
Kirill Kuznetsov <kka.fullstack@gmail.com>
2024-10-17 20:01:55 +0300

    .env

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,9 +1,12 @@
 import { env } from '~shared/lib/env';
 
 export function baseUrl(path: string) {
-  const url = env('VITE_API_URL', 'localhost');
+  const url = env('VITE_API_URL', 'no log');
   const port = env('VITE_API_PORT', '80');
   const method = port === '443' ? 'https' : 'http';
+
+  console.log(import.meta.env.PROD);
+  console.log(url);
 
   return `${method}://${url}:${port}/api${path}`;
 }
